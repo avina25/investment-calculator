2ffc9e639c81538ada5045e9684e992aa8c34c8e
avina25 <avinaalmeida9@gmail.com>
2025-10-01 00:07:36 +0530

    Added new feature: to calculate investment value

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,32 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import UserInput from './components/UserInput';
+import Results from './components/Results';
 export default function App() {
-  return <Header />;
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  const inputValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputValid && (
+        <p className='center'>Please enter a duration greater than zero.</p>
+      )}
+      {inputValid && <Results input={userInput} />}
+    </>
+  );
 }
